abb1c9eb326265e600718a9c9b69726c26045d7d
Enguerrand <enguerrand.pelletier@gmail.com>
2015-05-15 17:02:20 +0200

    * Added fields on model

--- api/models/user.js
+++ api/models/user.js
@@ -2,11 +2,23 @@ var User = mongoose.model("User", new mongoose.Schema({
   firstName       : { type : String,  required : true },
   lastName        : { type : String,  required : true },
   username        : { type : String,  required : true },
-  
-  openID          : { type : String }, 
+
+  openID          : { type : String },
   nbVideosViewed  : { type : Number,  default : 0 },
+
+  avatar          : {
+    data : { type: Buffer, required : true },
+    contentType : { type: String, required : true }
+  },
 
   deleted         : { type : Boolean, default : false },
   created         : { type : Date,    default : Date.now },
   modified        : { type : Date,    default : Date.now },
 }))
+
+var UserModel = function() {
+
+  this.model = User;
+  this.exposed = [] //TODO
+
+}
--- api/models/video.js
+++ api/models/video.js
@@ -2,12 +2,21 @@
 var Video = mongoose.model("Video", new mongoose.Schema({
     title       : { type : String,  required : true },
 
-    author      : { type : mongoose.Schema.ObjectId, ref : "User" },
+    author      : { type : mongoose.Schema.ObjectId, ref : "User", required: true },
 
     description : { type : String },
     views       : { type : Number,  default : 0 },
     access      : { type : Number,  default : 0 },
     shared_nb   : { type : Number,  default : 0 },
+
+    is_online       : { type: Boolean, default: false },
+    current_state   : { type: Number, default: 0 },
+    confidentiality : { type: Number, default: 0 },
+
+    cover       : {
+      data : { type: Buffer, required: true },
+      contentType : {type: String, required : true }
+    },
 
     deleted     : { type : Boolean, default : false },
     created     : { type : Date,    default : Date.now },
@@ -18,6 +27,19 @@ var VideoModel = function() {
 
   this.exposed =  ['title', 'author', 'description', 'views', 'access', 'shared_nb', 'created', 'modified'];
   this.model = Video;
+
+
+  this.STATE_CHOICE = {
+    "NEW" : 0,
+    "UPLOADED" : 1,
+    "AVAILABLE" : 2
+  }
+
+  this.CONFIDENTIALITY_CHOICE = {
+    "PUBLIC" : 0,
+    "PRIVATE_LINK" : 1,
+    "AUTH_USER" : 2
+  }
 
 }
 
