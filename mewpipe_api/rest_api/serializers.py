c1b61786e6531792e47bcc44d05e0e2098ae7d9e
Enguerrand <enguerrand.pelletier@gmail.com>
2015-05-18 22:38:59 +0200

    * Fixed: id are not displayed in nested m2m

--- mewpipe_api/rest_api/serializers.py
+++ mewpipe_api/rest_api/serializers.py
@@ -2,17 +2,27 @@ from rest_framework import serializers
 from rest_api.models import Video, Tag
 from rest_api.shortcuts import HtmlCleanField
 
+
+class TagSerializer(serializers.ModelSerializer):
+  videos = serializers.SlugRelatedField(
+      slug_field = 'uid',
+      read_only = True,
+      many=True,
+  )
+  name   = HtmlCleanField(max_length=100 )
+
+  class Meta:
+    model = Tag
+    fields = Tag.serialized
+
+
 class VideoSerializer(serializers.ModelSerializer):
   title = HtmlCleanField(max_length=40)
   description = HtmlCleanField(required=False)
+  tags = TagSerializer(many=True, read_only=True)
 
   class Meta:
     model = Video
     fields = Video.serialized
     depth = 1
 
-class TagSerializer(serializers.ModelSerializer):
-  name = HtmlCleanField(max_length=100 )
-
-  class Meta:
-    model = Tag
