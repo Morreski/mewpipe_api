c65a0b7504bc7a19ea21ddbc14894672d19de57b
Enguerrand <enguerrand.pelletier@gmail.com>
2015-05-23 11:08:49 +0200

    * Refactoring

--- mewpipe_api/rest_api/controllers/video.py
+++ mewpipe_api/rest_api/controllers/video.py
@@ -81,42 +81,12 @@ class VideoControllerGeneral(generics.ListCreateAPIView):
       VideoTag.objects.create(tag=t,video=v, tag_level=0)
 
 
-class VideoControllerSpecific(View):
+class VideoControllerSpecific(generics.RetrieveUpdateDestroyAPIView):
 
-  @csrf_exempt
-  def dispatch(self, *args, **kwargs):
-    return View.dispatch(self, *args, **kwargs)
+  queryset = Video.objects.all()
+  serializer_class = VideoSerializer
+  lookup_field = 'uid'
 
-  def get(self, request, uid):
-    video = get_by_uid(Video, uid)
-    if video is None:
-      return JsonResponse({}, status=404)
-
-    s = VideoSerializer(video)
-    return JsonResponse(s.data)
-
-  def put(self, request, uid):
-    video = get_by_uid(Video, uid)
-    if video is None:
-      return JsonResponse({}, status=404)
-
-    data = JSONParser().parse(request)
-    serializer = VideoSerializer(video, data=data)
-
-    if not serializer.is_valid():
-      return JsonResponse(serializer.errors, status = 400)
-
-    serializer.save()
-
-    return JsonResponse(serializer.data)
-
-  def delete(self, request, uid):
-    video = get_by_uid(Video, uid)
-    if video is None:
-      return JsonResponse({}, status=404)
-
-    video.delete()
-    return JsonResponse({}, status=204)
 
 class ShareController(View):
 
