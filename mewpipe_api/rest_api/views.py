834d491469b70e29934e497736bcaf3492478f72
Enguerrand <enguerrand.pelletier@gmail.com>
2015-06-09 21:11:48 +0200

    * Add missing import

--- mewpipe_api/rest_api/views.py
+++ mewpipe_api/rest_api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 
